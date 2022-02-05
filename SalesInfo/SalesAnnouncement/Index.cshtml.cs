b6d68d9dbd8609e68a5550415f3c2346df1b7052
edris <eefathi@gmail.com>
2022-02-05 09:18:38 +0330

    added the search queries for the tags

--- SalesInfo/SalesAnnouncement/Index.cshtml.cs
+++ SalesInfo/SalesAnnouncement/Index.cshtml.cs
@@ -40,6 +40,10 @@ namespace kamjaService.Pages.SalesInfo.SalesAnnouncement
                         select m;
             var p_tags = from m in _context.ParentTag
                          select m;
+            var pg_tags = from m in _context.ProductGroupTag
+                         select m;
+            var pging_tags = from m in _context.ProductGroupingTag
+                         select m;
             var pr2 = sales.Distinct();
 
             IQueryable<SalesAnnouncements> prs = pr2;
@@ -48,38 +52,74 @@ namespace kamjaService.Pages.SalesInfo.SalesAnnouncement
                 String[] searchstrZ = searchString.Split(' ');
 
                 pr2 = pr2.Search(s => s.title).ContainingAll(searchstrZ).AsQueryable();
-                //prs = pr2.Where(s => s.AjzaKalaName.Contains(s.AjzaKalaName));
-                //////////////////////
-                 var records = p_tags.Where(x => x.tag_name.Contains("," + searchstrZ[0] + ",") ||
-                  x.tag_name.StartsWith(searchstrZ[0] + ",") ||
-                  x.tag_name.EndsWith("," + searchstrZ[0]) ||
-                  x.tag_name.Contains(searchstrZ[0])).ToList();
-
-                foreach (var s in records)
-                {
-                    Console.WriteLine(s.tag_name);
-                }
-                //////////////////////
-                var SalesAnnounceIds = _context.ParentTag.Search(s=>s.tag_name).ContainingAll(searchstrZ).Select(s=>s.SalesAnnouncementREF).ToListAsync();
+                pr2 = pr2.Where(s => s.title.Contains(s.title));
                 
-               foreach (var s in await SalesAnnounceIds)
-                {
-                    Console.WriteLine(s);
-                }
-            
+                var SalesAnnounceIds_parent = _context.ParentTag.Search(s=>s.tag_name).ContainingAll(searchstrZ).Select(s=>s).AsQueryable();
+                var SalesAnnounceIds_productGroup = _context.ProductGroupTag.Search(s => s.tag_name).ContainingAll(searchstrZ).Select(s => s).AsQueryable(); 
+                var SalesAnnounceIds_productGrouping = _context.ProductGroupingTag.Search(s => s.tag_name).ContainingAll(searchstrZ).Select(s => s).AsQueryable(); 
+
+                var parent_query = from s in _context.SalesAnnouncements
+                            join t in SalesAnnounceIds_parent
+                            on s.SalesAnnouncementId equals t.SalesAnnouncementREF
+                            select s;
+                var productgroup_query = from s in _context.SalesAnnouncements
+                                   join t in SalesAnnounceIds_productGroup
+                                   on s.SalesAnnouncementId equals t.SalesAnnouncementREF
+                                   select s;
+                var productgrouping_query = from s in _context.SalesAnnouncements
+                                   join t in SalesAnnounceIds_productGrouping
+                                   on s.SalesAnnouncementId equals t.SalesAnnouncementREF
+                                   select s;
+                //for salesannouncement_id
+                //IQueryable<SalesAnnouncements> salesNumberId_query;
+                //for (int i = 0 ; i < searchstrZ.Length ; i++) {
+                //    var id = 0;
+                //    Int32.TryParse(searchstrZ[i], out id);
+                //    salesNumberId_query = salesNumberId_query.Concat(from s in _context.SalesAnnouncements
+                //                                   where s.SalesAnnouncementId == id
+                //                                   select s);
+                //}
+
+                pr2 = pr2.Concat(parent_query);
+                pr2 = pr2.Concat(productgroup_query);
+                pr2 = pr2.Concat(productgrouping_query);
+
+
+
             }
 
-            SalesAnnouncements = await pr2.OrderByDescending(x=>x.SalesAnnouncementId).ToListAsync();
+            SalesAnnouncements = await pr2.OrderByDescending(x=>x.SalesAnnouncementId).Distinct().ToListAsync();
         }
 
-        //public async Task<FileResult> OnPostDownloadPdf(int id)
-        //{
-        //    var file = await _context.SalesAnnouncements.Where(x => x.Id == id).FirstOrDefault();
-        //    if (file == null)
-        //    {
-        //        //TODO
-        //    }
-        //    return File(file.ReportContent, "application/pdf", "test.pdf");
-        //}
+
     }
 }
+
+
+
+
+
+//prs = pr2.Where(s => s.AjzaKalaName.Contains(s.AjzaKalaName));
+//////////////////////
+// var records = p_tags.Where(x => x.tag_name.Contains("," + searchstrZ[0] + ",") ||
+//  x.tag_name.StartsWith(searchstrZ[0] + ",") ||
+//  x.tag_name.EndsWith("," + searchstrZ[0]) ||
+//  x.tag_name.Contains(searchstrZ[0])).ToList();
+
+//foreach (var s in records)
+//{
+//    Console.WriteLine(s.tag_name);
+//}
+//////////////////////
+///
+
+
+//public async Task<FileResult> OnPostDownloadPdf(int id)
+//{
+//    var file = await _context.SalesAnnouncements.Where(x => x.Id == id).FirstOrDefault();
+//    if (file == null)
+//    {
+//        //TODO
+//    }
+//    return File(file.ReportContent, "application/pdf", "test.pdf");
+//}
